28c596d2856857014a9061b1457288c5a6d1b798
saradagu <gummadisarada0@gmail.com>
2025-08-23 16:26:28 +0530

    Time: 0 ms (100%), Space: 43.7 MB (5.79%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+        
+    }
+    public static void main(String [] args){
+        int[] nums={1,3,5,6};
+        int target=5;
+        int result=searchInsert(nums,target);
+        System.out.println("Output:"+result);
+    }
+}
